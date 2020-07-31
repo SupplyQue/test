3dc5cd765bca2bc76d3cc3e1adcb381272ceeca6
Prathab916 <57803471+Prathab916@users.noreply.github.com>
2020-07-31 18:42:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+
+
+
+
 function login() {
 
-	window.alert("click you dumbass")
+	
+	var userEmail = document.getElementById("inputEmail").value;
+	
+	var userPass = document.getElementById("inputPassword").value;
 	// body...
+	//window.alert(userEmail+"this shit is crazy"+userPass);
+	
+
 }
+firebase.auth().signInWithEmailAndPassword(userEmail,userPass).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  window.alert("Error:fuck off")
+  // ...
+});
